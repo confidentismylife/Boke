f77da9abe0c5db817129fd94129344221377ae4c
pjw <1335407527@qq.com>
2024-07-25 18:28:39 +0800

    1233

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ const withVideos = require('next-videos');
 
 const nextConfig = {
     env: {
-        API_ADDRESS: "http://localhost:3000/api",
+        API_ADDRESS: "https://line-art-dog-boke-pjw1.vercel.app/",
     },
+    output: 'standalone',
 }
 
 module.exports = withVideos(nextConfig);
